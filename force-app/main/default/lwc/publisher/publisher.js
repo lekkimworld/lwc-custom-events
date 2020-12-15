899b4b10007b425d11c552a5d011f93048e2f4e5
schandlergarcia <stephan.garcia@salesforce.com>
2020-12-15 14:28:26 -0800

    updated style

--- force-app/main/default/lwc/publisher/publisher.js
+++ force-app/main/default/lwc/publisher/publisher.js
@@ -1,18 +1,21 @@
 import { LightningElement } from 'lwc';
 
 export default class Publisher extends LightningElement {
-    handleClick(){
-        console.log('firing event');
-        this.dispatchEvent(
-            new CustomEvent("message", {
-                detail: {
-                    message: 'hello'
-                },
-                bubbles: true,
-                composed: true
-            })
-        );
 
+    value;
+
+    handleChange(event){
+        this.value = event.target.value;
+    }
+
+    handleClick(){
+        let event = new CustomEvent('message',{
+            detail: {
+                value: this.value
+            },
+            bubbles:true
+        });
+        this.dispatchEvent(event);
     }
 
 }
--- force-app/main/default/lwc/subscriber/subscriber.js
+++ force-app/main/default/lwc/subscriber/subscriber.js
@@ -1,14 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class Subscriber extends LightningElement {
 
+   @track itemList = [];
+
     connectedCallback(){
         this.handleMessage = this.handleMessage.bind(this);
-        window.addEventListener("message", this.handleMessage, false);
+        window.addEventListener('message',this.handleMessage, false);
     }
 
     handleMessage(event){
-        console.log('message recieved')
+        let detail = event.detail.value;
+        this.itemList.push(detail);
+    }
+
+    get showItemList(){
+        return this.itemList.length >= 1;
     }
 
 }
