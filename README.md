b4c1933a57ac97df524c98e3270f7884ffbe538b
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2024-04-15 10:17:07 +0200

    Fixup repo to allow event to propagate from publisher component to subscriber component

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Custom Event Example
 
 This repo demonstrates how you can use custom events to communicate accross lightning web components.  
+
+## Deployment
+```
+sf org create scratch --set-default -f config/project-scratch-def.json
+sf project deploy start
+sf org assign permset -n Event_Propagation
+sf org open -p "/lightning/n/Event_Propagation"
+```
--- force-app/main/default/lwc/subscriber/subscriber.js
+++ force-app/main/default/lwc/subscriber/subscriber.js
@@ -4,17 +4,9 @@ export default class Subscriber extends LightningElement {
 
     @track itemList = [];
 
-    connectedCallback(){
-        window.addEventListener('message', this.handleMessage, false);
-    }
-
     handleMessage = (event) => {
         let detail = event.detail.value;
         this.itemList.push(detail);
-    }
-
-    disconnectedCallback(){
-        window.removeEventListener('message', this.handleMessage, false);
     }
 
     get showItemList(){
